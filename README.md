4761bac7db7f1065252550354dd71406a0493d86
Pier-Luc Michaud <pier-luc@otopod.ca>
2024-07-19 21:59:00 -0400

    Add the project

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# porkbun-ssl-fetcher
-A small docker container you can use to download your SSL certificate and put them where you need them
+# Porkbun SSL Fetcher
+
+A small docker container you can use to download your SSL certificate and put them where you need them.
+
+# Usage
+
+## Docker Compose
+
+```yaml
+services:
+  nginx:
+    restart: unless-stopped
+    image: nginx:latest
+    volumes:
+      - ssl-certificates:/certificates
+    ports:
+      - 80:80
+      - 443:443
+
+  porkbun-ssl-fetcher:
+    restart: unless-stopped
+    image: porkbun-ssl-fetcher
+    environment:
+      DOMAIN: ${DOMAIN}
+      PORKBUN_API_KEY: ${PORKBUN_API_KEY}
+      PORKBUN_SECRET_KEY: ${PORKBUN_SECRET_KEY}
+    volumes:
+      - ssl-certificates:/certificates
+```
+
+## Nginx Config
+
+```
+ssl_certificate /certificates/DOMAIN.chain.cert.pem;
+ssl_certificate_key /certificates/DOMAIN.private.key.pem;
+ssl_trusted_certificate /certificates/DOMAIN.intermediate.cert.pem;
+```
---
+++ main.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+const process = require("process");
+const fs = require("fs/promises");
+
+const { DOMAIN, PORKBUN_API_KEY, PORKBUN_SECRET_KEY } = process.env;
+
+if (!DOMAIN) throw new Error("Missing DOMAIN env variable.");
+if (!PORKBUN_API_KEY) throw new Error("Missing PORKBUN_API_KEY env variable.");
+if (!PORKBUN_SECRET_KEY) throw new Error("Missing PORKBUN_SECRET_KEY env variable.");
+
+async function updateCertificates() {
+  console.log(`[${new Date().toISOString()}] Updating certificates for ${DOMAIN}.`);
+
+  const response = await axios.default.post(
+    `https://api.porkbun.com/api/json/v3/ssl/retrieve/${DOMAIN}`,
+    {
+      apikey: PORKBUN_API_KEY,
+      secretapikey: PORKBUN_SECRET_KEY,
+    },
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  const { intermediatecertificate, certificatechain, privatekey, publickey } = response.data;
+
+  await fs.writeFile(`/certificates/${DOMAIN}.intermediate.cert.pem`, intermediatecertificate);
+  await fs.writeFile(`/certificates/${DOMAIN}.chain.cert.pem`, certificatechain);
+  await fs.writeFile(`/certificates/${DOMAIN}.private.key.pem`, privatekey);
+  await fs.writeFile(`/certificates/${DOMAIN}.public.key.pem`, publickey);
+
+  console.log(`[${new Date().toISOString()}] Certificates for ${DOMAIN} updated.`);
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function main() {
+  await updateCertificates();
+  console.log(`[${new Date().toISOString()}] Sleeping for a day before updating certificates again.`);
+  await sleep(24 * 60 * 60 * 1000);
+}
+
+main();
